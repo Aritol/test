315ceb6f64c500df99db01315d738452c48b7893
Aritol <maxdurant2001@gmail.com>
2021-10-15 02:11:04 +0300

    add new code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("asdsadsadsada");
+        int a = 0;
+        System.out.println("Hello world");
     }
 }
